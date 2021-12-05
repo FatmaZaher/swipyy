5407926baa84f2ea1426cfbcd2073331fd9b8024
FatmaZaher <fatoomzaher1999@gmail.com>
2021-12-05 20:48:41 +0200

    add

--- src/component/MobileSide.jsx
+++ src/component/MobileSide.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import mobileCover from "../assets/images/mobile-cover.png";
 import personal from "../assets/images/personal.png";
 import ShareIcon from "@mui/icons-material/Share";
@@ -8,6 +8,8 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link } from "react-router-dom";
 import FormikControl from "../component/form/FormikControl";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import CancelIcon from "@mui/icons-material/Cancel";
+
 import * as Yup from "yup";
 
 const initialValues = {
@@ -19,66 +21,84 @@ const validationSchema = Yup.object({
 });
 
 const MobileSide = () => {
+  const [mobileSide, setMobileSide] = useState(false);
+  const showMobileSide = () => setMobileSide(!mobileSide);
   return (
-    <div className="mobile-side">
-      <img src={mobileCover} alt="" />
-      <div className="mobile-side-content">
-        <div className="personl-img mb-3">
-          <img src={personal} alt="" />
-        </div>
-        <div className="username mb-3">FahadMuhayya</div>
-        <div className="description mb-3">
-          FahadMuhayya FahadMuhayya FahadMuhayya
-        </div>
-        <div className="links">
-          <p className="mb-3">
-            <a href="#">sewarsa.com</a>
-            <ShareIcon />
-          </p>
-          <p className="mb-3">
-            <a href="#">sewarsa.com</a>
-            <ShareIcon />
-          </p>
-        </div>
-        <div className="social-links">
-          <a href="#">
-            <WhatsAppIcon />
-          </a>
-          <a href="#">
-            <FacebookIcon />
-          </a>
-        </div>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {(formik) => (
-            <Form className="mobiel-form">
-              <FormikControl
-                control="input"
-                type="text"
-                name="name"
-                placeholder="your name"
-              />
-              <FormikControl
-                control="input"
-                type="email"
-                name="email"
-                placeholder="your email"
-              />
-              <button type="submit" className="mobile-button">
-                Send Message
-              </button>
-            </Form>
-          )}
-        </Formik>
+    <>
+      <div className="icon-view">
+        <button type="button" className="link-button" onClick={showMobileSide}>
+          <RemoveRedEyeIcon />
+        </button>
       </div>
-      <Link to="/" className="link upgrad-btn">
-        <RemoveRedEyeIcon />
-        View Live
-      </Link>
-    </div>
+      <div
+        className={mobileSide ? "overlay show" : "overlay"}
+        onClick={showMobileSide}
+      ></div>
+      <div className={mobileSide ? "mobile-side show" : "mobile-side"}>
+        <div className="close-icon">
+          <button type="button" className="link-button" onClick={showMobileSide}>
+            <CancelIcon />
+          </button>
+        </div>
+        <img src={mobileCover} alt="" />
+        <div className="mobile-side-content">
+          <div className="personl-img mb-3">
+            <img src={personal} alt="" />
+          </div>
+          <div className="username mb-3">FahadMuhayya</div>
+          <div className="description mb-3">
+            FahadMuhayya FahadMuhayya FahadMuhayya
+          </div>
+          <div className="links">
+            <p className="mb-3">
+              <a href="#">sewarsa.com</a>
+              <ShareIcon />
+            </p>
+            <p className="mb-3">
+              <a href="#">sewarsa.com</a>
+              <ShareIcon />
+            </p>
+          </div>
+          <div className="social-links">
+            <a href="#">
+              <WhatsAppIcon />
+            </a>
+            <a href="#">
+              <FacebookIcon />
+            </a>
+          </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {(formik) => (
+              <Form className="mobiel-form">
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="name"
+                  placeholder="your name"
+                />
+                <FormikControl
+                  control="input"
+                  type="email"
+                  name="email"
+                  placeholder="your email"
+                />
+                <button type="submit" className="mobile-button">
+                  Send Message
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <Link to="/" className="link upgrad-btn">
+          <RemoveRedEyeIcon />
+          View Live
+        </Link>
+      </div>
+    </>
   );
 };
 export default MobileSide;
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import SidebarData from "./SidebarData";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
@@ -10,6 +10,8 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import PollOutlinedIcon from "@mui/icons-material/PollOutlined";
 import PaymentOutlinedIcon from "@mui/icons-material/PaymentOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const Sidebar = () => {
   const SidebarData = [
@@ -39,41 +41,59 @@ const Sidebar = () => {
       link: "/",
     },
   ];
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
-    <div className="sidebar">
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+    <>
+      <div className="icon-mobile">
+        <button type="button" className="link-button" onClick={showSidebar}>
+          <MenuIcon />
+        </button>
       </div>
-      <ul className="sidebarList">
-        {SidebarData.map((val, key) => {
-          return (
-            <li key={key} className="list-item">
-              <Link to={val.link}>
-                <div className="icon">{val.icon}</div>
-                <div className="title">{val.title}</div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <div className="upgrad">
-        <img src={vectary} alt="logo" />
-        <div className="box">
-          <p className="text">Updrade to pro for More features</p>
-          <Link to="/" className="link upgrad-btn">
-            Upgrade
+      <div
+        className={sidebar ? "overlay show" : "overlay"}
+        onClick={showSidebar}
+      ></div>
+      <div className={sidebar ? "sidebar show" : "sidebar"}>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
           </Link>
+          <div className="close-icon">
+            <button type="button" className="link-button" onClick={showSidebar}>
+              <CancelIcon />
+            </button>
+          </div>
+        </div>
+        <ul className="sidebarList">
+          {SidebarData.map((val, key) => {
+            return (
+              <li key={key} className="list-item">
+                <Link to={val.link}>
+                  <div className="icon">{val.icon}</div>
+                  <div className="title">{val.title}</div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="upgrad">
+          <img src={vectary} alt="logo" />
+          <div className="box">
+            <p className="text">Updrade to pro for More features</p>
+            <Link to="/" className="link upgrad-btn">
+              Upgrade
+            </Link>
+          </div>
+        </div>
+        <div className="shap1">
+          <img src={shap1} alt="logo" />
+        </div>
+        <div className="shap2">
+          <img src={shap2} alt="logo" />
         </div>
       </div>
-      <div className="shap1">
-        <img src={shap1} alt="logo" />
-      </div>
-      <div className="shap2">
-        <img src={shap2} alt="logo" />
-      </div>
-    </div>
+    </>
   );
 };
 export default Sidebar;
